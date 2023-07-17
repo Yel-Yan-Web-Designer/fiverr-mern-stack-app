a1028924bd0ecebc94db8d734dbf377f83d2a533
[Yel Yan] <[kmin2957@gmail.com]>
2023-07-17 19:39:47 +0300

    build orders & add page UI

--- client/src/pages/add/Add.jsx
+++ client/src/pages/add/Add.jsx
@@ -3,7 +3,74 @@ import "./Add.scss"
 
 const Add = () => {
   return (
-    <div>Add</div>
+    <div className='add'>
+      <div className="container">
+        <h1 className='add-title'>Add New Gig</h1>
+        <div className="add-section">
+          {/* left */}
+          <div className="left info">
+            <div className="left-title">
+              <label htmlFor="">Title</label>
+              <input type="text" placeholder="e.g. I will do something I'm really good at"/>
+            </div>
+            <div className="left-category">
+              <label htmlFor="">Category</label>
+              <select name="categories" id="categories">
+                <option value="design">Design</option>
+                <option value="web">Web Development</option>
+                <option value="animation">Animation</option>
+                <option value="music">Music</option>
+              </select>
+            </div>
+            <div className="left-cover">
+              <label htmlFor="">Cover Image</label>
+              <input type="file" />
+            </div>
+            <div className="left-upload-file">
+              <label htmlFor="">Upload Images</label>
+              <input type="file" multiple/>
+            </div>
+            <div className="left-desc">
+              <label htmlFor="">Description</label>
+              <textarea name="" id="" placeholder='Brief descriptions to introduce your service to customers'></textarea>
+            </div>
+          </div>
+          {/* right */}
+          <div className="right details">
+            <div className="right-title">
+              <label htmlFor="">Service Title</label>
+              <input type="text" placeholder='e.g. One-page web design'/>
+            </div>
+            <div className="right-desc">
+              <label htmlFor="">Short Description</label>
+              <textarea name="" id="" placeholder="Short description of your service"  cols="30" rows="10"></textarea>
+            </div>
+            <div className="right-deli">
+              <label htmlFor="">Delivery Time (e.g. 3 days)</label>
+              <input type="number" />
+            </div>
+            <div className="right-revision">
+              <label htmlFor="">Revision Number</label>
+              <input type="number" />
+            </div>
+            <div className="right-features">
+              <label htmlFor="">Features</label>
+              <input type="text" placeholder="e.g. page design" />
+              <input type="text" placeholder="e.g. file uploading" />
+              <input type="text" placeholder="e.g. setting up a domain" />
+              <input type="text" placeholder="e.g. hosting" />
+            </div>
+            <div className="right-price">
+              <label htmlFor="">Price</label>
+              <input type="number" />
+            </div>
+            <button className="create-btn">Create</button>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/orders/Orders.jsx
+++ client/src/pages/orders/Orders.jsx
@@ -2,8 +2,78 @@ import React from 'react';
 import "./Orders.scss"
 
 const Orders = () => {
+
+  const currentUser = {
+    id : 1,
+    username : "Anna",
+    isSeller : true
+  }
+
   return (
-    <div>Orders</div>
+    <div className='orders'>
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+
+        {/* table */}
+        <div className="table-container">
+          <table>
+            <tr>
+              <th><h3>Image</h3></th>
+              <th><h3>Title</h3></th>
+              <th><h3>Price</h3></th>
+              <th><h3>Orders</h3></th>
+              <th><h3>Action</h3></th>
+            </tr>
+            <tr>
+              <td className='img-td'>
+                <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" className='mygigs-image' />
+              </td>
+              <td>Ai generated concept art</td>
+              <td>120.<sup>90</sup></td>
+              <td>41</td>
+              <td className='img-td'> 
+                <img className="delete" src="/img/delete.png" alt="" />
+              </td>
+            </tr>
+            <tr>
+              <td className='img-td'>
+                <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" className='mygigs-image' />
+              </td>
+              <td>Ai generated concept art</td>
+              <td>120.<sup>90</sup></td>
+              <td>41</td>
+              <td className='img-td'>
+                <img className="delete" src="/img/delete.png" alt="" />
+              </td>
+            </tr>
+            <tr>
+              <td className='img-td'>
+                <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" className='mygigs-image' />
+              </td>
+              <td>Ai generated concept art</td>
+              <td>120.<sup>90</sup></td>
+              <td>41</td>
+              <td className='img-td'>
+                <img className="delete" src="/img/delete.png" alt="" />
+              </td>
+            </tr>
+            <tr>
+              <td className='img-td'>
+                <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" className='mygigs-image' />
+              </td>
+              <td>Web design for responsive</td>
+              <td>110.<sup>20</sup></td>
+              <td>12</td>
+              <td className='img-td'>
+                <img className="delete" src="/img/delete.png" alt="" />
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
 
